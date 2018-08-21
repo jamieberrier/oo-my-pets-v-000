7959225de519c2dc3a3f8564fc01b49396906fd6
jamieberrier <berrier.jamie@gmail.com>
2018-08-21 16:00:49 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
-# An owner will know about all its pets, be able to buy a pet, set the name of a pet 
-# (which the pet can't change, because that would be weird), change a pet's mood through 
+# An owner will know about all its pets, be able to buy a pet, set the name of a pet
+# (which the pet can't change, because that would be weird), change a pet's mood through
 # walking, feeding, or playing with it, and sell all of its pets (for when it moves to that tiny NYC studio after college).
 require 'pry'
 
