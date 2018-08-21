572aa94053d0de28cbe637a579f8706f6fd59f63
jamieberrier <berrier.jamie@gmail.com>
2018-08-21 16:36:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -49,6 +49,9 @@ class Owner
     @pets
   end
 
+  # Make a new instance of the appropriate pet, initializing it with that name.
+  # Associate that new pet instance to the owner by adding it to the appropriate array-value of
+    # the @pets hash stored in the pets attr_accessor.
   def buy_fish(name)
   end
 end
