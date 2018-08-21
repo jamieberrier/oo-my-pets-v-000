518935a2c5dd6b72139ea11ae16943830dfb13ad
jamieberrier <berrier.jamie@gmail.com>
2018-08-21 16:08:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,6 @@
+# Make a new instance of the appropriate pet, initializing it with that name.
+# Associate that new pet instance to the owner by adding it to the appropriate array-value of 
+  # the @pets hash stored in the pets attr_accessor.
 class Dog
   # code goes here
 end
