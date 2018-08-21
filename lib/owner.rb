99152a758d84d63e9a3b6f9cdd40e2cced9dcada
jamieberrier <berrier.jamie@gmail.com>
2018-08-21 17:05:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -78,8 +78,8 @@ class Owner
   end
 
   def sell_pets
-      @pets[:dogs].each {|dog| dog.mood=("nervous")}
-      @pets[:cats].each {|cat| cat.mood=("nervous")}
-      @pets[:fishes].each {|fish| fish.mood=("nevous")}
+      # @pets[:dogs].each {|dog| dog.mood=("nervous")}
+      # @pets[:cats].each {|cat| cat.mood=("nervous")}
+      # @pets[:fishes].each {|fish| fish.mood=("nevous")}
   end
 end
