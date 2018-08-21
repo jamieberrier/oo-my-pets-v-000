981f67d2a26b88dd9a5448806ddad4402ca52419
jamieberrier <berrier.jamie@gmail.com>
2018-08-21 16:08:24 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,7 @@
 require 'pry'
 # Make a new instance of the appropriate pet, initializing it with that name.
-# Associate that new pet instance to the owner by adding it to the appropriate array-value of 
-# the @pets hash stored in the pets attr_accessor.
+# Associate that new pet instance to the owner by adding it to the appropriate array-value of
+  # the @pets hash stored in the pets attr_accessor.
 class Cat
   # code goes here
   attr_reader :name
